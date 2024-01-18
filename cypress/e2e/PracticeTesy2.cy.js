dcfafc1d18da7f29a577d670ae81abceeefc0011
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-01-18 21:39:24 +0100

    Update PracticeTesy2.cy.js

--- cypress/e2e/PracticeTesy2.cy.js
+++ cypress/e2e/PracticeTesy2.cy.js
@@ -1,11 +1,13 @@
 it('Visit Medium Website', () => {
     cy.visit('https://www.willys.se/')
     cy.wait(4000)
-    cy.get('#onetrust-reject-all-handler').click()
-    cy.get('[data-testid="horizontalbanner-link"] > .sc-dfa63f22-0')
-        .should('contain', 'Till maten')
-    cy.get('[data-testid="mini-cart-button"]').should('have.focus')
-    //should('have.class', 'sc-dfa63f22-0 gRPtcz sc-607c619f-4 bWdbZk')
-    //cy.get('#onetrust-accept-btn-handler').should('contain', 'Acceptera All cookies')
+    //cy.get('#onetrust-reject-all-handler').click()
 
+    cy.get('#onetrust-accept-btn-handler')
+        .should('be.visible')
+        .should('contain', 'Acceptera All cookies')
+
+    cy.get('[data-testid="horizontalbanner-link"] > .sc-dfa63f22-0')
+        .and('contain', 'Till maten') //chained assertion
+        .and('have.class', 'sc-dfa63f22-0 gRPtcz sc-607c619f-4 bWdbZk')
 })
