6361bb09a4b58e7e879ee02fb752740a6b6e6594
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-02-04 16:44:40 +0100

    Update EcommerceWebsiteTestAutomation.cy.js

--- cypress/e2e/EcommerceWebsiteTestAutomation.cy.js
+++ cypress/e2e/EcommerceWebsiteTestAutomation.cy.js
@@ -1,10 +1,22 @@
 describe('E-commerce Website Test Suite', () => {
   it('Visit E-commerce Website', () => {
     cy.visit('https://www.bstackdemo.com/')
+    cy.wait(1000)
+  })
+  it('Log into Website with Valid Credentials and Logout', () => {
+    cy.visit('https://www.bstackdemo.com/')
+    cy.wait(1000)
     cy.get('#signin').click()
-    cy.contains('Select Username')
-    cy.get('#react-select-2-option-0-0')
-    cy.contains('Select Password').type('testingisfun99')
+    cy.get('#username > .css-yk16xz-control > .css-1hwfws3')
+      .type('demouser{enter}')
+    cy.wait(1500)
+    cy.get('#password > .css-yk16xz-control > .css-1hwfws3')
+      .click()
+      .type('testingisfun99{enter}')
+    cy.wait(1500)
     cy.get('#login-btn').click()
+    cy.wait(1500)
+    cy.contains('Logout').click()
+
   })
 })
