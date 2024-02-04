a373621dee3d2566c1bc95644d0d8c75614d5da4
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-02-04 20:35:36 +0100

    update

--- cypress/e2e/PracticeTest1.cy.js
+++
@@ -1,8 +0,0 @@
-it('Visit Shein Website', () => {
-  cy.visit('https://www.shein.se/')
-  cy.get('.c-coupon-box > .iconfont').click()
-  cy.get('.div-input').type('Skirt{Enter}')
-  //cy.get('.search-btn > .suiiconfont-critical').click()
-  cy.contains('Beige').click()
-  cy.wait(2000)
-})
