acc1670c6164ca19b0ebaf9ed127d6d19c21941e
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-02-05 21:38:34 +0100

    product_add_cart

---
+++ cypress/e2e/mark_product_fav.cy.js
@@ -0,0 +1,18 @@
+describe('Product Sort', () => {
+    beforeEach(() => {
+        // Visit the homepage before each test
+        cy.visit('/')
+    });
+    context('Product add to cart', () => {
+
+        // Test case for filter products by selected brand tag (Apple)
+        it('Should add products into cart according to selection', () => {
+
+
+            // Assertions to verify selected products are added to cart
+
+
+        })
+    })
+
+})
