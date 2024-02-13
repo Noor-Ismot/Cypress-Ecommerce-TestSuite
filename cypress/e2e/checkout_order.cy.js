e4b10841d8ef448af8f86c2106c71642819d5d33
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-02-13 19:34:15 +0100

    Update checkout_order.cy.js

--- cypress/e2e/checkout_order.cy.js
+++ cypress/e2e/checkout_order.cy.js
@@ -4,9 +4,9 @@ describe('Product Sort', () => {
         cy.visit('/')
     });
     context('Product add to cart', () => {
+        it('Should add products into cart and checkout after authentication', () => {
 
-        // Test case to add product into cart
-        it('Should add products into cart according to selection', () => {
+            // Test case to add product into cart
             cy.contains('Add to cart').click()
             cy.contains('Add to cart').click()
 
@@ -15,9 +15,31 @@ describe('Product Sort', () => {
             cy.get('.desc')
                 .should('contain', 'Apple')
                 .should('contain', 'Quantity: 2')
+            cy.get('.buy-btn').click()
 
-            // Assertions to verify checkout
+            // Test steps to login with valid credentials before checkout
 
+            cy.get('#username > .css-yk16xz-control > .css-1hwfws3').type('demouser{enter}')
+            cy.get('#password > .css-yk16xz-control > .css-1hwfws3')
+                .click()
+                .type('testingisfun99{enter}')
+            cy.get('#login-btn').click()
+
+            // Assertions to verify checkout URL after successful Login
+            cy.url().should('eq', 'https://www.bstackdemo.com/checkout')
+
+            //Test Case to add details into checkout form
+            cy.get('#firstNameInput').type('Noor Ismot')
+            cy.get('#lastNameInput').type('Ara')
+            cy.get('#addressLine1Input').type('Sweden')
+            cy.get('#provinceInput').type('Gothenburg')
+            cy.get('#postCodeInput').type('45151')
+            cy.wait(2000)
+            cy.get('#checkout-shipping-continue').click()
+
+            // Assertions to verify checkout successful
+            cy.url().should('eq', 'https://www.bstackdemo.com/confirmation')
+            cy.get('.checkout-steps').should('contain', 'Your Order has been successfully placed')
 
         })
     })
